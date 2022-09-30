0c9a23e04c75849dfb09ddcaae1c8f3981b8d880
Sajan M <sajanmurali@ymail.com>
2022-09-30 18:24:05 +0530

    inial commit

---
+++ project.py
@@ -0,0 +1,9 @@
+from cProfile import label
+from tkinter import *
+root = Tk()
+root.geometry("500x300")
+
+Label(root, text="Python Programming Registration Form", font="arial 15 bold").grid(row=0, column=3)
+
+
+root.mainloop()
